d2c6772ae9f77dcab4ede6e6909dfb5b02ee1a1f
MuhammadMuddassir <39741315+MuhammadMuddassir@users.noreply.github.com>
2019-11-21 03:09:51 +0100

    Update Camera Estimation .m

--- Camera Estimation .m
+++ Camera Estimation .m
@@ -1,10 +1,10 @@
 
-%% Camera Estimation from a Calibration Object
+% Camera Estimation from a Calibration Object
 
 function [X_norm, T] = Norm3Dpoint(X)
-%% Number of Points
+% Number of Points
 no_pts = size(X, 2);
-%% Normalize points for estimation (isotropic scaling)
+% Normalize points for estimation (isotropic scaling)
 % move centroid to zero
 Centr = mean(X(1:3, :), 2);
 X_cent = bsxfun(@minus, X(1:3, :), Centr);
@@ -19,7 +19,7 @@ T(1:3, 4) = -s*Centr;
 return % end of Norm2Dpoint
 
 function P = EstimPNormML(Ximg, Xwrld, Pest)
-%% Initialization and Normalization
+% Initialization and Normalization
 [Ximg_norm, T1] = Norm2Dpoint(Ximg); % Perfect points
 [Xwrld_norm, T2] = Norm3Dpoint(Xwrld); % Measurement Space: N = 3n
 Pest_norm = T1*Pest*T2^-1;
@@ -28,10 +28,10 @@ p0 = reshape(Pest_norm', 1, []); % initial estimate of P
 [p_opt, ~, ~, ~, ~] = LMopt(p0, Ximg_norm, Xwrld_norm);
 Popt_tilde = reshape(p_opt, 4, 3)';
 Popt_tilde = Popt_tilde./Popt_tilde(3, 3);
-%% Denormalization
+% Denormalization
 P = T1^-1*Popt_tilde*T2;
 
-%% Denormalization
+% Denormalization
 P = T1^-1*Popt_tilde*T2;
 function [x, resnorm, residual, exitflag, output] = LMopt(x0, Ximg, Xwrld)
 options = optimset('MaxFunEvals', 5000, ...
